49135e62ea46996996d29b9814fbcb3876886a5e
ali <ali.shadowhite@gmail.com>
2018-12-12 02:12:36 +0600

    product group migration

--- database/migrations/2018_12_11_193216_create_product_groups_table.php
+++ database/migrations/2018_12_11_193216_create_product_groups_table.php
@@ -15,6 +15,16 @@ class CreateProductGroupsTable extends Migration
     {
         Schema::create('product_groups', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('short_name')->nullable();
+            $table->text('description')->nullable();
+            $table->Integer('company_id')->unsigned()->nullable();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->integer('creator_user_id')->unsigned();
+            $table->foreign('creator_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('updator_user_id')->unsigned()->nullable();
+            $table->foreign('updator_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
